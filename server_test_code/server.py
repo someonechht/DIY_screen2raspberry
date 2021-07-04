74fd273a34fb760860b21060aef348cb34ba867c
Claus Huynh <claus.huynh@yahoo.dk>
2021-07-04 12:28:00 +0200

    server noe receive pic and show

--- server_test_code/server.py
+++ server_test_code/server.py
@@ -5,28 +5,30 @@ import socket
 from PIL import Image
 from io import BytesIO
 
-def client_thread(client_socket):
-	bytesize = 6220800
-	run = True
-	stream = BytesIO()
-#	client_socket.recvmsg_into(bytesize)
-	while run:
-		data = bytearray(bytesize)
-		client_socket.recvmsg_into([data])
-		stream.write(data)
-		
-		image = Image.fromarray(data)
-	#print(image)
-	
-	
 
-ip = '192.168.10.166' #socket.gethostbyname(socket.gethostname())
+
+def client_thread(client_socket):
+	width = int(client_socket.recv(1024))
+	height = int(client_socket.recv(1024))
+	data = bytearray()
+	while len(data) < width*height*4:
+		data += client_socket.recv(width*height*4)
+		if not data:
+			break
+		print(len(data))
+	img = Image.frombytes("RGB", (width, height), bytes(data), "raw", "BGRX")
+	img.show()
+
+	
+	
+ip = socket.gethostbyname(socket.gethostname()) #'192.168.10.214' #socket.gethostbyname(socket.gethostname())
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((ip, 1024))
-server_socket.listen(1)
+server_socket.listen(1) #listen to 1 client
 print(ip)
 while True:
 	( client_socket, address ) = server_socket.accept()
 	ct = client_thread(client_socket)
-	ct.run()
+	if(ct != None):
+		ct.run()
 	
